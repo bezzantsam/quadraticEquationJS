25bd12153aa4109901d5c2f1b03fea6866c051ac
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2021-10-30 20:50:28 -0700

    basic quadratic equation in js

---
+++ const numa = 1;.js
@@ -0,0 +1,7 @@
+const numa = 1;
+const numx = 10;
+const numb = 10; 
+const numc = 13.56;
+
+console.log(Math.pow(numx, 2) * numa + (numb * numx) + numc);
+/* the a in this formula is to the right of the x^2 part of the formula. Functions properly*/ 
