553b736c606b54f681bbdc1b2385293fab26861c
Sam Bezzant <83626945+bezzantsam@users.noreply.github.com>
2021-10-30 20:52:34 -0700

    Delete const numa = 1;.js

--- const numa = 1;.js
+++
@@ -1,7 +0,0 @@
-const numa = 1;
-const numx = 10;
-const numb = 10; 
-const numc = 13.56;
-
-console.log(Math.pow(numx, 2) * numa + (numb * numx) + numc);
-/* the a in this formula is to the right of the x^2 part of the formula. Functions properly*/ 
